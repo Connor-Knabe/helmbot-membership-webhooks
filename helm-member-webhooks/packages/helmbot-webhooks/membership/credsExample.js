1e368e636d447684a0b4c10fd0da3de6deb40dcb
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-10-13 14:53:47 -0500

    interactive slack message

--- helm-member-webhooks/packages/helmbot-webhooks/membership/credsExample.js
+++ helm-member-webhooks/packages/helmbot-webhooks/membership/credsExample.js
@@ -2,3 +2,7 @@ module.exports.giphyAPIKey="asdfasdfasdfasdf";
 module.exports.slackWebhookURL="https://hooks.slack.com/services/INFOHERE/INFOHERE/INFOHERE";
 module.exports.slackChannelID="INFOHERE";
 module.exports.botID="INFOHERE";
+module.exports.helmToSlackNames = {
+    "John Smith":"<@LSKDJFJSDF>"
+};
+module.exports.helmbotUsersUrl = "https://yourshopnamehere.floathelm.com/users/"
--- helm-member-webhooks/packages/helmbot-webhooks/membership/helmbot-membership-webhooks.js
+++ helm-member-webhooks/packages/helmbot-webhooks/membership/helmbot-membership-webhooks.js
@@ -1,48 +1,97 @@
 const got = require("got");
 const creds = require("./creds");
-//Isn't verifying webhook is valid from Slack
 
 async function main(params) {
     console.log('params',params);
-
-    // var data = JSON.parse(MakerWebhooks.jsonEvent.JsonPayload);
-
-    if(params?.membership-range-begun != undefined && params?.membership_type_name){
-        const membershipType = params["membership_type_name"];
-        const soldByUsername = params["sold_by_user_name"];
-    
-        const helmToSlackNames = {
-            "Anna Jones":"<@UP9F1FSMU>",
-            "Ashley Stout":"<@U01T7TZ701G>",
-            "Claire Forshee":"<@U03GH2H06SH>",
-            "Connor Knabe":"<@U0FTYG2HE>",
-            "Jenna Burton":"<@UGXHR7GP6>",
-            "Kiley Phillips":"<@U03310WKE6B>",
-            "Noah Barnes":"<@U2B5GLR1Q>",
-            "Zac Crismaru":"<@UK3972UN6>"
-        };
-
-        const soldBy = soldByUsername == undefined ? "Online store" : helmToSlackNames[soldByUsername];
-
-        var slackMessage = "Wooohooo!! "+ soldBy + " sold a "+ membershipType + "!";
-        Slack.postToChannel.setMessage(slackMessage);
-
-        const giphy = await got('https://api.giphy.com/v1/gifs/random?api_key='+creds.giphyAPIKey+'&tag=awesome').json();
-
-        const {data} = await got.post(creds.slackWebhookURL, {
-            json: {
-                channel: creds.slackChannelID,
-                text: giphy?.data?.url
-            }
-        }).json();
-        
-        return {
-            statusCode: 200,
-            headers: { 'Content-Type': 'application/json' },
-            body: {"challenge":params.challenge}
-        };
-
+    if(params["event_type"] == "membership-range-begun" && params["membership_type_name"] != undefined){
+        return await membershipSold(params);
     }
-  }
+    if(params["event_type"] == "reservation-checkout" && params["checkout_by_user_name"] != undefined && params["service_type_title"] == "Float"){
+        return await customerFloatCheckout(params);
+    }
+}
+
+async function customerFloatCheckout(params) {
+    const customerName = params["customer_name"];
+    const checkedOutBy = getEmployee(params);
+    const customerId = params["customer_id"];
+    const customerNameAndUrl = `<${creds.helmbotUsersUrl}${customerId}| ${customerName}>`;
+
+    var slackMessage = `*Reminder for:*  ${checkedOutBy} you checked out ${customerNameAndUrl}.  \n*Add notes to ${customerNameAndUrl}'s profile:*\n • Why they float?\n• Did you present membership opportunity?\n• If they didn't sign up why not?\n\nFeel free to start a thread on this post to brainstorm tips for next time.`;
+
+
+    await got.post(creds.slackWebhookURL, {
+        json: {
+            channel: creds.slackChannelID,
+            blocks: [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": slackMessage
+                    }
+                },{
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "checkboxes",
+                            "options": [
+                                {
+                                    "text": {
+                                        "type": "mrkdwn",
+                                        "text": "*Check this box when you've added notes to helm*"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    });
+
+    return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: {"challenge":params.challenge}
+    };
+
+}
+
+async function membershipSold(params) {
+    const membershipType = params["membership_type_name"];
+    const soldBy = getEmployee(params);
+
+    var slackMessage = "Wooohooo!! "+ soldBy + " sold a "+ membershipType + "!";
+
+    await got.post(creds.slackWebhookURL, {
+        json: {
+            channel: creds.slackChannelID,
+            text: slackMessage
+        }
+    });
+    const giphy = await got('https://api.giphy.com/v1/gifs/random?api_key='+creds.giphyAPIKey+'&tag=awesome').json();
+
+    await got.post(creds.slackWebhookURL, {
+        json: {
+            channel: creds.slackChannelID,
+            text: giphy?.data?.url
+        }
+    });
+    
+    return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: {"challenge":params.challenge}
+    };
+}
+
+
+function getEmployee(params){
+    const soldByUsername = params["sold_by_user_name"];
+    const checkoutByUsername = params["checkout_by_user_name"];
+    const employee = soldByUsername == undefined ? creds.helmToSlackNames[checkoutByUsername] : creds.helmToSlackNames[soldByUsername];
+    return employee;
+}
 
 exports.main = main
