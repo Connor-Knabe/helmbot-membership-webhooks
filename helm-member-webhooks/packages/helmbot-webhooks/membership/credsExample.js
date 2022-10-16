196f0a1f979f041b8c99aa66a7bfba2460627c78
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-10-16 14:25:25 -0500

    updating creds example

--- helm-member-webhooks/packages/helmbot-webhooks/membership/credsExample.js
+++ helm-member-webhooks/packages/helmbot-webhooks/membership/credsExample.js
@@ -1,7 +1,7 @@
-module.exports.giphyAPIKey="asdfasdfasdfasdf";
-module.exports.slackWebhookURL="https://hooks.slack.com/services/INFOHERE/INFOHERE/INFOHERE";
-module.exports.slackChannelID="INFOHERE";
-module.exports.botID="INFOHERE";
+module.exports.giphyAPIKey= "asdfasdfasdfasdf";
+module.exports.slackWebhookURL= "https://hooks.slack.com/services/INFOHERE/INFOHERE/INFOHERE";
+module.exports.slackDebugWebhookURL= "https://hooks.slack.com/services/INFOHERE/INFOHERE/INFOHERE";
+module.exports.slackWebhookSalesUrl = "https://hooks.slack.com/services/INFOHERE/INFOHERE/INFOHERE";
 module.exports.helmToSlackNames = {
     "John Smith":"<@LSKDJFJSDF>"
 };
