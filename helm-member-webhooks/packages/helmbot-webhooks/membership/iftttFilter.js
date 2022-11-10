4024da1fa50b00d4c03099638dda6861ad50ffaf
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-11-10 14:05:38 -0600

    updating ifttt filter

--- helm-member-webhooks/packages/helmbot-webhooks/membership/iftttFilter.js
+++ helm-member-webhooks/packages/helmbot-webhooks/membership/iftttFilter.js
@@ -1,37 +1,36 @@
-var data = JSON.parse(MakerWebhooks.jsonEvent.JsonPayload);
-var occurredAt = MakerWebhooks.jsonEvent.OccurredAt;
+const data = JSON.parse(MakerWebhooks.jsonEvent.JsonPayload);
+const occurredAt = MakerWebhooks.jsonEvent.OccurredAt;
+const serviceType = data["service_type_title"];
+const eventType = data["event_type"];
+const membershipTypeName = data["membership_type_name"] != undefined ? data["membership_type_name"].toLowerCase() : "";
+const employeeName = data["checkout_by_user_name"] != undefined ? data["checkout_by_user_name"] : data["sold_by_user_name"];
+const customerName = data["customer_name"] != undefined ? data["customer_name"] : "N/a";
+const pastReservationCount = data["customer_past_reservation_count"] != undefined ? data["customer_past_reservation_count"] : "N/a";
+const endsAt = data["ends_at"] != undefined ? data["ends_at"] : "N/a";
+const activeMembershipCount = data["location_memberships_active_count"] != undefined ? data["location_memberships_active_count"] : 0;
+const membershipsNeedingCardCount = data["location_memberships_active_needing_card_count"] != undefined ? data["location_memberships_active_needing_card_count"] : 0;
 
-var serviceType = data["service_type_title"];
-var eventType = data["event_type"];
-var membershipTypeName = data["membership_type_name"] != undefined ? data["membership_type_name"].toLowerCase() : "";
-
-var employeeName = data["checkout_by_user_name"] != undefined ? data["checkout_by_user_name"] : data["sold_by_user_name"];
-
-var customerName = data["customer_name"] != undefined ? data["customer_name"] : "N/a";
-
-var pastReservationCount = data["customer_past_reservation_count"] != undefined ? data["customer_past_reservation_count"] : "N/a";
-
-var endsAt = data["ends_at"] != undefined ? data["ends_at"] : "N/a";
-
-
-var activeMembershipCount = data["location_memberships_active_count"] != undefined ? data["location_memberships_active_count"] : 0;
-var membershipsNeedingCardCount = data["location_memberships_active_needing_card_count"] != undefined ? data["location_memberships_active_needing_card_count"] : 0;
-
+var saleName = "";
 var membershipCount = 0;
 
 if (activeMembershipCount !=0 && membershipsNeedingCardCount != 0){
   membershipCount = activeMembershipCount - membershipsNeedingCardCount;
 }
 
+var startEndMembership = undefined; 
+if(eventType == "membership-range-begun"){
+    startEndMembership = "Started";
+} else if (eventType == "membership-range-ended"){
+    startEndMembership = "Ended";
+}
 
-var saleName = "";
-if (eventType == "membership-range-begun") {
+if (startEndMembership != undefined) {
     if (membershipTypeName.includes("float")) {
-        saleName = "Float Membership";
+        saleName = `Float Membership ${startEndMembership}`;
     } else if (membershipTypeName.includes("massage")) {
-        saleName = "Massage Membership";
+        saleName = `Massage Membership ${startEndMembership}`;
     } else if (membershipTypeName.includes("sauna")) {
-        saleName = "Sauna Membership";
+        saleName = `Sauna Membership ${startEndMembership}`;
     }
 } else if (eventType == "reservation-checkout") {
     saleName = `${serviceType} Session`;
