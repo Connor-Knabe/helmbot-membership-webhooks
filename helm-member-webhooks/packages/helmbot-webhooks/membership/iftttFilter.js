ce68b7ffd118b501be64eb8b3455b9be222d7c4a
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2023-01-20 13:30:35 -0600

    updating ifttt filter

--- helm-member-webhooks/packages/helmbot-webhooks/membership/iftttFilter.js
+++ helm-member-webhooks/packages/helmbot-webhooks/membership/iftttFilter.js
@@ -6,6 +6,7 @@ const eventType = data["event_type"];
 const membershipTypeId = data["membership_type_id"] != undefined ? data["membership_type_id"] : "";
 
 
+const serviceTitle = data["service_title"] != undefined ? data["service_title"] : "";
 
 const employeeName = data["checkout_by_user_name"] != undefined ? data["checkout_by_user_name"] : data["sold_by_user_name"];
 // const customerName = data["customer_name"] != undefined ? data["customer_name"] : "N/a";
@@ -16,7 +17,7 @@ const membershipsPastDueCount = data["location_memberships_active_past_due_count
 
 const customerId = data["customer_id"] != undefined ? data["customer_id"] : "N/a";
 
-const checkoutByUserId = data["checkout_by_user_id"] != undefined ? data["checkout_by_user_id"] : "N/a";
+var checkoutByUserId = data["checkout_by_user_id"] != undefined ? data["checkout_by_user_id"] : "N/a";
 
 const pricePaid = data["price_paid"] != undefined ? data["price_paid"] : "N/a";
 
@@ -37,8 +38,10 @@ var membershipOrServiceId = "";
 if(eventType == "membership-range-begun"){
     checkoutOrMembership = "Membership Started";
     membershipOrServiceId = membershipTypeId;
+    checkoutByUserId = data["sold_by_user_id"] != undefined ? data["sold_by_user_id"] : "N/a";
 } else if (eventType == "membership-range-ended"){
     checkoutOrMembership = "Membership Ended";
+    checkoutByUserId = data["sold_by_user_id"] != undefined ? data["sold_by_user_id"] : "N/a";
     membershipOrServiceId = membershipTypeId;
 } else if(serviceTypeId){
     checkoutOrMembership = "Session"
@@ -47,4 +50,4 @@ if(eventType == "membership-range-begun"){
 
 
 
-GoogleSheets.appendToGoogleSpreadsheet.setFormattedRow(`${new Date().toDateString()} ||| ${checkoutByUserId} ||| ${customerId} ||| ${checkoutOrMembership} ||| ${membershipOrServiceId} ||| ${membershipCount} ||| ${pastReservationCount}`);
+GoogleSheets.appendToGoogleSpreadsheet.setFormattedRow(`${new Date().toDateString()} ||| ${checkoutByUserId} ||| ${customerId} ||| ${checkoutOrMembership} ||| ${membershipOrServiceId} ||| ${membershipCount} ||| ${pastReservationCount} ||| ${activeMembershipCount} ||| ${sessionStartTime} ||| ${serviceTitle}`);
